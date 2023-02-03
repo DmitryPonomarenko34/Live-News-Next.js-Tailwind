06469387d4cd9816da39086b0f10ca6638f7c43c
DmitryPonomarenko34 <dimaponomarenko443@gmail.com>
2023-02-03 22:13:56 +0200

    implementong stepzen

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,13 @@
-import React from "react";
+import { categories } from "../constants"
 
-export default function HomePage() {
+export default async function HomePage() {
+  // const news: NewsResponse = await fetchNews(categories.join(','));
+
+  // console.log(news);
+  
+
+  
+  http://api.mediastack.com/v1/news?access_key=771f00aa3a7def73eee263dcd4d0105d
   return (
     <div>Home page</div>
   )
---
+++ lib/fetchNews.ts
@@ -0,0 +1,9 @@
+const fetchNews = async (
+  category?: Categories | string,
+  keywords?: string,
+  isDynamic?: boolean,
+) => {
+  
+} 
+
+// stepzen import curl http://api.mediastack.com/v1/news?access_key=771f00aa3a7def73eee263dcd4d0105d&sort=published_desc&limit=25&languages=en

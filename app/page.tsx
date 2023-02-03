e067a35b16ff884f85f9d7905931561ce00df178
DmitryPonomarenko34 <dimaponomarenko443@gmail.com>
2023-02-04 00:54:40 +0200

    build the fetch news function

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,11 @@
+import { NewspaperIcon } from "@heroicons/react/24/solid";
 import { categories } from "../constants"
+import { fetchNews } from "../lib/fetchNews";
 
 export default async function HomePage() {
   // const news: NewsResponse = await fetchNews(categories.join(','));
-
   // console.log(news);
-  
 
-  
-  http://api.mediastack.com/v1/news?access_key=771f00aa3a7def73eee263dcd4d0105d
   return (
     <div>Home page</div>
   )
--- lib/fetchNews.ts
+++ lib/fetchNews.ts
@@ -1,9 +1,75 @@
-const fetchNews = async (
+import { gql } from "graphql-request"
+import sortNewsByImg from "./sortNewsByImg";
+
+export const fetchNews = async (
   category?: Categories | string,
   keywords?: string,
   isDynamic?: boolean,
 ) => {
-  
-} 
+  const query = gql`
+    query myQuery(
+      $access_key: String!
+      $categories: String!
+      $keywords: String
+    ) {
+      myQuery(
+        access_key: $access_key
+        categories: $categories
+        countries: "us"
+        sort: "published_desc"
+        keywords: $keywords
+      ) {
+        pagination {
+          count
+          limit
+          offset
+          total
+        }
+        data {
+          author
+          category
+          country
+          description
+          image
+          language
+          published_at
+          source
+          title
+          url
+        }
+    }
+  }
+  `;
 
-// stepzen import curl http://api.mediastack.com/v1/news?access_key=771f00aa3a7def73eee263dcd4d0105d&sort=published_desc&limit=25&languages=en
+  const response = await fetch('https://jhingergacha.stepzen.net/api/zeroed-iguana/__graphql', {
+    method: 'POST',
+    cache: isDynamic ? 'no-cache' : 'default',
+    next: isDynamic ? { revalidate: 0 } : { revalidate: 20 },
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `APIKey ${process.env.STEPZEN_API_KEY}`
+    },
+    body: JSON.stringify({
+      query,
+      variables: {
+        access_key: process.env.MEDIA_STACK_API_KEY,
+        categories: category,
+        keywords: keywords,
+      }
+    })
+  });
+
+  console.log(
+    'data >>',
+    category,
+    keywords,
+  );
+
+  const newsResponse = await response.json();
+  console.log(newsResponse);
+
+  
+  const news = sortNewsByImg(newsResponse.data.myQuery);
+
+  return news
+}
